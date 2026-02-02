6e2e9950c5056c56ef967c20b9657f61f9b2027a
David Panesiu <davidpanesiu@gmail.com>
2026-02-01 22:45:14 -0500

    Complete temperature exercise in grade script

--- 05-grade.sh
+++ 05-grade.sh
@@ -19,3 +19,15 @@ fi
 # that prints "it's cold" if the temperature is < 40
 # it's chilly if < 60, it's okay if < 70 and, it's hot for 
 # everything else
+echo "What is the current temperature?"
+read temp
+
+if [ $temp -lt 40 ]; then
+   echo "it's cold"
+elif [ $temp -lt 60 ]; then
+   echo "it's chilly"
+elif [ $temp -lt 70 ]; then
+   echo "it's okay"
+else
+   echo "it's hot"
+fi
